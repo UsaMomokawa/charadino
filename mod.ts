a6b8167333fb0f53d98b4a7cda742db2282539f4
UsaMomokawa <42172002+UsaMomokawa@users.noreply.github.com>
2023-07-15 18:54:34 +0900

    ファイルへの書き込みは行わない

--- mod.ts
+++ mod.ts
@@ -1,19 +1,15 @@
-import { fastFileLoader, startBot } from "./deps.ts";
+import { startBot } from "./deps.ts";
 import { updateApplicationCommands } from "./src/utils/updateCommands.ts";
 import { Bot } from "./bot.ts";
 import { logger } from "./src/utils/logger.ts";
 
-logger.info("Starting bot... 🚀");
+import "./src/commands/check.ts";
+import "./src/commands/mod.ts";
+import "./src/events/interactionCreate.ts";
+import "./src/events/mod.ts";
+import "./src/events/ready.ts";
 
-const paths = [
-  "./src/events",
-  "./src/commands",
-];
-await fastFileLoader(paths).catch((error) => {
-  logger.fatal(`Unable to import ${paths}`);
-  logger.fatal(error);
-  Deno.exit(1);
-});
+logger.info("Starting bot... 🚀");
 
 await updateApplicationCommands();
 
