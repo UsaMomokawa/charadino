b2da09660d933b11ba55b41084dda5345913a367
UsaMomokawa <42172002+UsaMomokawa@users.noreply.github.com>
2023-07-15 23:32:26 +0900

    不要

--- src/utils/updateCommands.ts
+++ src/utils/updateCommands.ts
@@ -1,15 +1,7 @@
 import { Bot } from "../../bot.ts";
-import { ENV } from "../../secret.ts";
 
 export async function updateApplicationCommands() {
   await Bot.helpers.upsertGlobalApplicationCommands(
-    Bot.commands
-      .filter((command) => !command.devOnly)
-      .array(),
-  );
-
-  await Bot.helpers.upsertGuildApplicationCommands(
-    ENV["DISCORD_GUILD_ID"],
     Bot.commands
       .filter((command) => !command.devOnly)
       .array(),
